6d837bf25c39f261de24b93f92e28a6fdb40fe4c
jongwony <lastone9182@gmail.com>
2020-06-02 00:41:18 +0900

    init receiver

---
+++ app/src/main/java/com/example/smslistener/SmsListener.java
@@ -0,0 +1,23 @@
+package com.example.smslistener;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+
+public class SmsListener extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                String messageBody = smsMessage.getMessageBody();
+                Toast.makeText(context, intent.getAction() + " : " + messageBody, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(context, intent.getAction(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
